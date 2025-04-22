06c8e8b8a62c8b01ebc9cf36d3f47e699c8d977c
Parth <parthbatwara@gmail.com>
2025-04-22 15:52:54 +0530

    frontend ready to deploy

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 export async function fetchContacts() {
-  const res = await fetch("http://localhost:3000/api/contacts", {
-    method: "GET",
-  }); // adjust port if needed
+  const res = await fetch(
+    "https://hubspot-api-integration.onrender.com/api/contacts",
+    {
+      method: "GET",
+    }
+  ); // adjust port if needed
   const data = await res.json();
   console.log(data);
   return data;
